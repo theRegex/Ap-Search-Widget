891be22af3cec5fe5bff4ffcd160f3e5c14411f4
theRegex <keinan.jpace@gmail.com>
2016-05-09 16:29:32 -0500

    updated controller

--- list.controller.js
+++ list.controller.js
@@ -1,10 +1,13 @@
 apSearch.controller('list', ['$scope','apCall', function($scope,apCall){
 	$scope.msg = "Hello Welcome to List View";
 	$scope.dataResults = null
+	$scope.props = null;
+	
 	 apCall.processRequestedLocation().
 		then(function(res){
 			//sucess
 			$scope.dataResults = angular.fromJson(res)
+			$scope.props = $scope.dataResults.properties;
 			console.log($scope.dataResults + ": Response result")
 		}, function(e){
 			//error
